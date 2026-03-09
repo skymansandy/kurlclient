078df63743ce94c849e59ae7b015b424dbad5d4c
P Sandesh Baliga <iamsandythedev@gmail.com>
2026-03-09 20:01:21 +0530

    chore: Fix README (#4)
    * Update Readme
    
    * Add toolbar icons

--- composeApp/src/jvmMain/kotlin/dev/skymansandy/kurlclient/main.kt
+++ composeApp/src/jvmMain/kotlin/dev/skymansandy/kurlclient/main.kt
@@ -1,8 +1,17 @@
 package dev.skymansandy.kurlclient
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import dev.skymansandy.kurlclient.di.initKoin
+import dev.skymansandy.kurlclient.presentation.screens.workspace.rememberWorkspaceDesktopActions
+import dev.skymansandy.kurlclient.presentation.theme.KurlTheme
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
 
 fun main() {
     initKoin()
@@ -11,7 +20,34 @@ fun main() {
             onCloseRequest = ::exitApplication,
             title = "KurlClient",
         ) {
+            val workspaceActions = rememberWorkspaceDesktopActions()
+            var showAboutDialog by remember { mutableStateOf(false) }
+
+            MenuBar {
+                Menu("Workspace") {
+                    Item("Import cURL") {
+                        workspaceActions.onImportCurl()
+                    }
+                    Item("Export cURL") {
+                        val curl = workspaceActions.onExportCurlCommand()
+                        Toolkit.getDefaultToolkit().systemClipboard
+                            .setContents(StringSelection(curl), null)
+                    }
+                }
+                Menu("Help") {
+                    Item("About KurlClient") {
+                        showAboutDialog = true
+                    }
+                }
+            }
+
             App()
+
+            if (showAboutDialog) {
+                KurlTheme {
+                    AboutDialog(onDismiss = { showAboutDialog = false })
+                }
+            }
         }
     }
 }
